7771f98999681545e36a6550b67d1aa7e80a2164
Rahmat Rasyidi Hakim <rahmat.rasyidi@hotmail.com>
2021-09-28 21:00:06 +0700

    add samples

---
+++ samples/app/src/main/java/com/example/suhuf/SampleFragment.kt
@@ -0,0 +1,4 @@
+package com.example.suhuf
+
+class SampleFragment {
+}
--- samples/app/src/main/java/com/example/suhuf/sheet/SampleSheet.kt
+++ samples/app/src/main/java/com/example/suhuf/sheet/SampleSheet.kt
@@ -1,5 +1,9 @@
 package com.example.suhuf.sheet
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import com.example.suhuf.R
 import com.rahmatrasyidi.suhuf.Suhuf
 import com.rahmatrasyidi.suhuf.data.PeekHeight
@@ -11,7 +15,31 @@ class SampleSheet : Suhuf(R.layout.sheet_sample) {
     override val isDraggable: Boolean get() = true
     override val peekHeight: PeekHeight get() = PeekHeight.HEIGHT_FULL
 
+    override fun extractArguments(bundle: Bundle?) {
+        super.extractArguments(bundle)
+        val message = bundle?.getString(KEY_MESSAGE).orEmpty()
+        showToast(message)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnPositive = view.findViewById<Button>(R.id.btnPositive)
+        btnPositive.setPositiveButtonClicked()
+
+        val btnNegative = view.findViewById<Button>(R.id.btnNegative)
+        val bundle = Bundle().apply {
+            putString(KEY_MESSAGE, "Message from bundle!")
+        }
+        btnNegative.setNegativeButtonClicked(bundle)
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+    }
+
     companion object {
         const val IDENTIFIER = "sampleSheet"
+
+        const val KEY_MESSAGE = "message"
     }
 }
